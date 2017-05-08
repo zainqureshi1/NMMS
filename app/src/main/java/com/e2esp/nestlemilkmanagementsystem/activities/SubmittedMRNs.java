b31fff8a32e6f82f3f8df48c3c58daad48632cc8
asadmumtaz <asad.mumtaz@e2esp.com>
2017-05-08 17:33:52 +0500

    Design UI of Submitted MRNs
    In this user interface all the submitted mrns will show.

--- app/src/main/java/com/e2esp/nestlemilkmanagementsystem/activities/SubmittedMRNs.java
+++ app/src/main/java/com/e2esp/nestlemilkmanagementsystem/activities/SubmittedMRNs.java
@@ -1,7 +1,7 @@
 package com.e2esp.nestlemilkmanagementsystem.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.e2esp.nestlemilkmanagementsystem.R;
 
